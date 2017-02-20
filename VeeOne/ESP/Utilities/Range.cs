03e08b56b71515d13b281dc4e02f1f1d5d66a516
johnpdowling <john.patrick.dowling@gmail.com>
2017-02-20 10:50:31 -0800

    second commit
    Add Range Utility

--- VeeOne/ESP/Utilities/Range.cs
+++ VeeOne/ESP/Utilities/Range.cs
@@ -1,10 +1,58 @@
 ﻿using System;
-namespace VeeOne
+namespace VeeOne.Utilities
 {
 	public class Range
 	{
+		public int LoFreq;
+		public int HiFreq;
+
+		public Range(int loFreq, int hiFreq)
+		{
+			LoFreq = loFreq;
+			HiFreq = hiFreq;
+		}
+
 		public Range()
 		{
+			LoFreq = 0;
+			HiFreq = 0;
+		}
+
+		public double Width
+		{
+			get
+			{
+				return HiFreq - LoFreq;
+			}
+		}
+
+		public Range Clone()
+		{
+			return new Range(LoFreq, HiFreq);
+		}
+
+		public bool IsContained(Range r)
+		{
+			return IsContained(r.LoFreq, r.HiFreq);
+		}
+
+		public bool IsContained(int _loFreq, int _hiFreq)
+		{
+			if ((_loFreq >= LoFreq) && (_loFreq <= HiFreq) && (_hiFreq >= LoFreq) && (_hiFreq <= HiFreq))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool IsContained(int frequency)
+		{
+			if ((frequency >= this.LoFreq) && (frequency <= this.HiFreq))
+			{
+				return true;
+			}
+			return false;
 		}
 	}
 }
